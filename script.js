c56a7493d6af1dbfe809b55cd084d19cebb21874
piaspro <golamrahman09@gmail.com>
2020-07-28 08:46:23 +0600

    almost finished

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// Making the notification disable
+// Making the notification invisible
 document.getElementById("matchNotification").style.display = "none"
 document.getElementById("misMatchNotification").style.display = "none"
+document.getElementById("action-left").style.display = "none"
 // Generating random Number
-function generateRandom(){
-    document.getElementById('generated-num').value = Math.floor(Math.random()*8999+1000);
+function generateRandom() {
+    document.getElementById('generated-num').value = Math.floor(Math.random() * 8999 + 1000);
 }
 // ACTIVE THE NUMBER BUTTONS
 function numberButton(num) {
@@ -12,21 +13,36 @@ function numberButton(num) {
 // active the delete button
 function deleteBtn() {
     let inputValue = document.getElementById('password-input').value;
-    document.getElementById('password-input').value = inputValue.substring(0,inputValue.length-1);
+    document.getElementById('password-input').value = inputValue.substring(0, inputValue.length - 1);
 }
 // active the clear button
 function clearEverything() {
     document.getElementById('password-input').value = "";
 }
 // Active the submit Button
+let count = 0;
+
 function submitButton() {
-   let inputNumber = document.getElementById('password-input').value;
-   let generatedNumber = document.getElementById('generated-num').value;
-   if (inputNumber == generatedNumber) {
-    document.getElementById("matchNotification").style.display = "block"
-    document.getElementById("misMatchNotification").style.display = "none"
-   } else {
-    document.getElementById("matchNotification").style.display = "none"
-    document.getElementById("misMatchNotification").style.display = "block"
-   }
+    let inputNumber = document.getElementById('password-input').value;
+    let generatedNumber = document.getElementById('generated-num').value;
+    if (inputNumber == "" && generatedNumber == "") {
+        alert('Please Generate Pin First');
+    } else if (inputNumber == generatedNumber) {
+        document.getElementById("matchNotification").style.display = "block"
+        document.getElementById("misMatchNotification").style.display = "none"
+    } else {
+        document.getElementById("matchNotification").style.display = "none"
+        document.getElementById("misMatchNotification").style.display = "block"
+    }
+    count++
+    if (count == 1 && inputNumber !== generatedNumber) {
+        document.getElementById("action-left").style.display = "block"
+    } else if (count == 2 && inputNumber !== generatedNumber) {
+        document.getElementById("action-left").innerText = "2 try left"
+    } else if (count == 3 && inputNumber !== generatedNumber) {
+        document.getElementById("action-left").innerText = "1 try left"
+    } else if (count == 4 && inputNumber !== generatedNumber) {
+        document.getElementById("action-left").innerText = "0 try left"
+        document.getElementById("submit-button").disabled = true;
+    }
 }
